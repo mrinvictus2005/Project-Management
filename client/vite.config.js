c74d7e2868cc562fb8e2b0d805393e1ce5799305
Alok Gupta <alokgupta49849@gmail.com>
2026-05-23 12:01:26 +0530

    Fixed production proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: "http://localhost:8800",
+                target: "https://veyraflow-production-c87c.up.railway.app",
                 changeOrigin: true,
             },
         },
